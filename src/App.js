d040cb2835acdc51dd568da476b11ec37142f842
Tishocngu <Nvt2632002work@gmail.com>
2023-04-05 16:41:18 +0700

    add all file

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./App.css"
+
+import RegisterPage from "./RegisterAcount/RegisterAcount";
+
+function App(){
+    return <RegisterPage />
+}
+export default App;
